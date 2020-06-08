4ea896acb10966c4aca040052cebc8f6362b5b7f
yuuta sumiyoshi <xkool52@gmail.com>
2020-06-08 10:49:44 +0000

    validation

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        // メッセージ一覧を取得
+        $tasks = Task::all();
+
+        // メッセージ一覧ビューでそれを表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function create()
+    {
+        $task = new Task;
+
+        // メッセージ作成ビューを表示
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        // バリデーション
+        $this->validate($request, [
+            'content' => 'required|max:255',
+            'status' => 'required|max:10',
+        ]);
+
+        // メッセージを作成
+        $task = new Task;
+        $task->content = $request->content;
+        $task->status = $request->status;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    public function show($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ詳細ビューでそれを表示
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // バリデーション
+        $this->validate($request, [
+            'content' => 'required|max:255',
+            'status' => 'required|max:10',
+        ]);
+        
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを更新
+        $task->content = $request->content;
+        $task->status = $request->status;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    public function destroy($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+}
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                    <th>ステータス</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+
+
+@endsection
