f78a875349f34d2b93dd5f632ab75e5ea1a7cdea
yuuta sumiyoshi <xkool52@gmail.com>
2020-06-18 02:47:33 +0000

    add column

--- database/migrations/2020_06_13_160937_add_user_to_tasks_table.php
+++ database/migrations/2020_06_13_160937_add_user_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddUserToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->nullable()->change();
+            $table->unsignedBigInteger('user_id');
 
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users');
